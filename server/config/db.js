8d785d0ce7f3898808acfda1180c7219f43b352e
Sharmaine <sapps1305@gmail.com>
2022-10-27 20:25:48 -0400

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  URI: "mongodb://127.0.0.1:27017/book_store",
+   //URI: "mongodb+srv://mengo1:abcd1234@cluster1.esn4h8r.mongodb.net/book_store?retryWrites=true&w=majority",
+};
+
