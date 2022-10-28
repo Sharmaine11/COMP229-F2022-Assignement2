73f9498b6c768411d0830db931b8b0ef20ca7664
Sharmaine <sapps1305@gmail.com>
2022-10-28 00:40:48 -0400

    updated URI to work with MongoDB atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  URI: "mongodb://127.0.0.1:27017/book_store",
-   //URI: "mongodb+srv://mengo1:abcd1234@cluster1.esn4h8r.mongodb.net/book_store?retryWrites=true&w=majority",
+  //URI: "mongodb://127.0.0.1:27017/book_store",
+   URI: "mongodb+srv://sam:meng17184JEAY@server1.jca64wq.mongodb.net/?retryWrites=true&w=majority",
 };
 
